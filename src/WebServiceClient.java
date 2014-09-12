539c47701bccf5cdd1b143c16877ba80e991c11a
hariAtThinkBright <hari@thinkbrightmedia.com.au>
2014-09-13 01:04:00 +1000

    Implemented faults into client

--- src/WebServiceClient.java
+++ src/WebServiceClient.java
@@ -1,17 +1,23 @@
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import au.edu.unsw.sltf.client.ImportDownloadFaultException;
 import au.edu.unsw.sltf.client.ImportDownloadServicesStub;
 import au.edu.unsw.sltf.services.DownloadFileDocument;
-import au.edu.unsw.sltf.services.DownloadFileResponseDocument;
-import au.edu.unsw.sltf.services.ImportMarketDataDocument;
-import au.edu.unsw.sltf.services.ImportMarketDataResponseDocument;
 import au.edu.unsw.sltf.services.DownloadFileDocument.DownloadFile;
+import au.edu.unsw.sltf.services.DownloadFileResponseDocument;
 import au.edu.unsw.sltf.services.DownloadFileResponseDocument.DownloadFileResponse;
+import au.edu.unsw.sltf.services.ImportDownloadFaultDocument;
+import au.edu.unsw.sltf.services.ImportDownloadFaultDocument.ImportDownloadFault;
+import au.edu.unsw.sltf.services.ImportMarketDataDocument;
 import au.edu.unsw.sltf.services.ImportMarketDataDocument.ImportMarketData;
+import au.edu.unsw.sltf.services.ImportMarketDataResponseDocument;
 import au.edu.unsw.sltf.services.ImportMarketDataResponseDocument.ImportMarketDataResponse;
+import au.edu.unsw.sltf.services.impl.ImportDownloadFaultDocumentImpl;
+import au.edu.unsw.sltf.services.impl.ImportDownloadFaultDocumentImpl.ImportDownloadFaultImpl;
 
 /*
  * To change this template, choose Tools | Templates
@@ -43,22 +49,45 @@ public class WebServiceClient {
         ImportMarketData req = reqDoc.addNewImportMarketData();
         req.setDataSourceURL("http://www.cse.unsw.edu.au/~hpaik/9322/assignments/common/files_csv_spec/FinDataSimple.csv");
         
-        Date d = new SimpleDateFormat("dd/mm/yyyy").parse("29/06/2001");
+        Date d = null;
+        try {
+        	d = new SimpleDateFormat("dd/mm/yyyyThh:mm:ss.SS").parse("29/06/2001T12:00:00.00");
+        } catch(ParseException e) {
+        	//TODO
+        	return null;
+        	//throw new ImportDownloadFaultException("Invalid start date");
+        }
         Calendar s = Calendar.getInstance();
         s.setTime(d);
         req.setStartDate(s);
         
         req.setSec("ABCD");
         
-        d = new SimpleDateFormat("dd/mm/yyyy").parse("01/08/2001");
+        try {
+        	d = new SimpleDateFormat("dd/mm/yyyyThh:mm:ss.SS").parse("01/08/2001T12:00:00.00");
+        } catch(ParseException e) {
+        	//TODO
+        	return null;
+        	//throw new ImportDownloadFaultException("Invalid end date");
+        }
         Calendar e = Calendar.getInstance();
         e.setTime(d);
         req.setEndDate(e);
 
-        // Use the stub (from generated code) to make the call.
-        ImportMarketDataResponseDocument respDoc = stub.importMarketData(reqDoc);
-        ImportMarketDataResponse resp = respDoc.getImportMarketDataResponse();
-        String result = resp.getEventSetId();
+        String result = "";
+        try {
+        	ImportMarketDataResponseDocument respDoc = stub.importMarketData(reqDoc);
+            ImportMarketDataResponse resp = respDoc.getImportMarketDataResponse();
+            result += resp.getEventSetId();
+        } catch (ImportDownloadFaultException ie) {
+        	ImportDownloadFaultDocument faultDoc = ie.getFaultMessage();
+        	ImportDownloadFault idf = faultDoc.getImportDownloadFault();
+        	au.edu.unsw.sltf.services.ImportDownloadFaultType.Enum faultType = idf.getFaultType();
+        	String faultMsg = idf.getFaultMessage();
+        	
+        	result += "Fault type: " + faultType + "\n";
+        	result += "Fault message: " + faultMsg;
+        }
 
         return result;
     }
@@ -71,10 +100,21 @@ public class WebServiceClient {
         req.setEventSetId("3");
 
         // Use the stub (from generated code) to make the call.
-        DownloadFileResponseDocument respDoc = stub.downloadFile(reqDoc);
-        DownloadFileResponse resp = respDoc.getDownloadFileResponse();
-        String result = resp.getDataURL();
-
+        String result = "";
+        try {
+        	DownloadFileResponseDocument respDoc = stub.downloadFile(reqDoc);
+        	DownloadFileResponse resp = respDoc.getDownloadFileResponse();
+        	result += resp.getDataURL();
+        } catch (ImportDownloadFaultException ie) {
+        	ImportDownloadFaultDocument faultDoc = ie.getFaultMessage();
+        	ImportDownloadFault idf = faultDoc.getImportDownloadFault();
+        	au.edu.unsw.sltf.services.ImportDownloadFaultType.Enum faultType = idf.getFaultType();
+        	String faultMsg = idf.getFaultMessage();
+        	
+        	result += "Fault type: " + faultType + "\n";
+        	result += "Fault message: " + faultMsg;
+        }
+        
         return result;
     }
 }
